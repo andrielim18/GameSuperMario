7795933eb7045d920b7102bd81b35bd223c62d25
Andri <andrielim18@gmail.com>
2022-06-28 10:27:37 -0300

    Adição de áudio

--- index.js
+++ index.js
@@ -3,48 +3,43 @@ const pipe = document.querySelector(".pipe-game");
 //const pontos = document.querySelector('pontos');
 const textStart = document.getElementById('text-start');
 const pontos = document.querySelector(".Pontos");
+const music = document.querySelector(".trilhaSonora");
+const gameOverMusic = document.querySelector(".gameOverMusic");
 
+/*Contagem de Pontos*/
+const contarPontos = pontos;
+let score = 0;
+const contador = setInterval(() => {
+  score++;
+  contarPontos.innerHTML = `Pontos: ${score}`;
+},500);
+
+/*Função Pulo*/
+function jump() {
+  textStart.style.display = "none";
+  mario.classList.add("jump-mario");
+
+  setTimeout(() => {
+    mario.classList.remove("jump-mario");
+  }, 500);
+
+}
 
 /*Função Start*/
 function start(jump) {
   pipe.classList.add('pipe-animation');
   mario.src = './imagens/super-mario.gif';
   mario.style.width = '150px';
+  
 
 };
 
 
-
-/*Função Pulo*/
-function jump() {
-  textStart.style.display="none";
-  mario.classList.add("jump-mario");
-
-  setTimeout(() => {
-    mario.classList.remove("jump-mario");
-  }, 500);
-}
-
-/*Função Contagem de Pontos*/ 
-const contarPontos = pontos;
-let score = 0;
-
-const contador = setInterval((()=>{
-  score ++;
-  contarPontos.innerHTML =`Pontos: ${score}`;
-  
-}
-
-),500);
-
-
-
-
 /*Verficar se bateu*/
-
 const loopGame = setInterval(() => {
   const pipePosition = pipe.offsetLeft;
   const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+  music.play();
 
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
     pipe.style.animation = "none";
@@ -57,13 +52,15 @@ const loopGame = setInterval(() => {
     mario.style.width = "75px";
     mario.style.marginLeft = "45px";
 
-    
+    music.volume=0.0;
+    gameOverMusic.play();
 
-    textStart.style.display="inline-block";
-    textStart.style.backgroundColor="red";
-    textStart.style.fontSize="50px";
+    textStart.style.display = "inline-block";
+    textStart.style.backgroundColor = "red";
+    textStart.style.fontSize = "50px";
     document.getElementById("text-start").innerHTML = "GAME OVER";
-    
+
+
     clearInterval(contador);
     clearInterval(loopGame);
 
